59322e8e10f16a8da46ecf3768a1f6e5094ed4cc
Daniel Leonte <leontedaniel37@gmail.com>
2023-11-13 15:58:32 +0200

    add cors origin

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const errorMiddleware = require("./middleware/errorMiddleware");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgres API" });
